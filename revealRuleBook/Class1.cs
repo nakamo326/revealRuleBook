3730dcdf5aae85e322472a9ce9292f9b4cf96467
Ynakamot <nakamo326@gmail.com>
2021-11-01 21:58:20 +0900

    プロジェクト ファイルを追加します。

---
+++ revealRuleBook/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using DiskCardGame;
+
+namespace testMod
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "nakamo326.inscryption.revealRuleBook";
+        private const string PluginName = "RevealRuleBook";
+        private const string PluginVersion = "1.0.0";
+        void Awake()
+        {
+            var harmony = new Harmony(PluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(RuleBookInfo))]
+    [HarmonyPatch("FillStatIconPage")]
+    public class RevarlRuleBookPatch
+    {
+        static bool Prefix(RuleBookPageInfo page, int iconIndex)
+        {
+            SpecialStatIcon iconIndex2 = (SpecialStatIcon)iconIndex;
+            page.pageId = iconIndex2.ToString();
+            return false;
+        }
+    }
+}
+
